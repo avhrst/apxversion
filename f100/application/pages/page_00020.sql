5e348f961178c86053d7bad1d674189d24b00d17
Oleksii Vykhristiyk <avhrst@gmail.com>
2021-10-01 09:00:21 +0300

    init

--- f100/application/pages/page_00020.sql
+++ f100/application/pages/page_00020.sql
@@ -32,7 +32,7 @@ prompt APPLICATION 100 - App1
 --   Flashback:       0
 --   Export Type:     Page Export
 --   Manifest
---     PAGE: 1
+--     PAGE: 20
 --   Manifest End
 --   Version:         21.1.0
 --   Instance ID:     1000155761403694
@@ -40,40 +40,6 @@ prompt APPLICATION 100 - App1
 
 begin
 null;
-end;
-/
-prompt --application/pages/delete_00001
-begin
-wwv_flow_api.remove_page (p_flow_id=>wwv_flow.g_flow_id, p_page_id=>1);
-end;
-/
-prompt --application/pages/page_00001
-begin
-wwv_flow_api.create_page(
- p_id=>1
-,p_user_interface_id=>wwv_flow_api.id(26251251977397106)
-,p_name=>'Home'
-,p_alias=>'HOME'
-,p_step_title=>'App1'
-,p_autocomplete_on_off=>'OFF'
-,p_page_template_options=>'#DEFAULT#'
-,p_last_updated_by=>'TEST'
-,p_last_upd_yyyymmddhh24miss=>'20211001085231'
-);
-wwv_flow_api.create_page_plug(
- p_id=>wwv_flow_api.id(26262013095397178)
-,p_plug_name=>'App1'
-,p_icon_css_classes=>'app-icon'
-,p_region_template_options=>'#DEFAULT#'
-,p_escape_on_http_output=>'Y'
-,p_plug_template=>wwv_flow_api.id(26151858071397040)
-,p_plug_display_sequence=>10
-,p_plug_display_point=>'REGION_POSITION_01'
-,p_plug_query_num_rows=>15
-,p_attribute_01=>'N'
-,p_attribute_02=>'HTML'
-,p_attribute_03=>'Y'
-);
 end;
 /
 prompt --application/end_environment
