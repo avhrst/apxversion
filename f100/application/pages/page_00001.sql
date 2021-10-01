dac528ee3f9d82f86e3dc7190bdb97627fd74dbe
Oleksii Vykhristiyk <avhrst@gmail.com>
2021-10-01 11:11:24 +0300

    import app

--- f100/application/pages/page_00001.sql
+++ f100/application/pages/page_00001.sql
@@ -1,9 +1,18 @@
-prompt --application/pages/page_00001
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- ORACLE Application Express (APEX) export file
+--
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_210100 or as the owner (parsing schema) of the application.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
 begin
---   Manifest
---     PAGE: 00001
---   Manifest End
-wwv_flow_api.component_begin (
+wwv_flow_api.import_begin (
  p_version_yyyy_mm_dd=>'2021.04.15'
 ,p_release=>'21.1.0'
 ,p_default_workspace_id=>4200481731648079
@@ -11,6 +20,35 @@ wwv_flow_api.component_begin (
 ,p_default_id_offset=>0
 ,p_default_owner=>'VCONTROL'
 );
+end;
+/
+ 
+prompt APPLICATION 100 - App1
+--
+-- Application Export:
+--   Application:     100
+--   Name:            App1
+--   Exported By:     SYSTEM
+--   Flashback:       0
+--   Export Type:     Page Export
+--   Manifest
+--     PAGE: 1
+--   Manifest End
+--   Version:         21.1.0
+--   Instance ID:     1000155761403694
+--
+
+begin
+null;
+end;
+/
+prompt --application/pages/delete_00001
+begin
+wwv_flow_api.remove_page (p_flow_id=>wwv_flow.g_flow_id, p_page_id=>1);
+end;
+/
+prompt --application/pages/page_00001
+begin
 wwv_flow_api.create_page(
  p_id=>1
 ,p_user_interface_id=>wwv_flow_api.id(26251251977397106)
@@ -20,7 +58,7 @@ wwv_flow_api.create_page(
 ,p_autocomplete_on_off=>'OFF'
 ,p_page_template_options=>'#DEFAULT#'
 ,p_last_updated_by=>'TEST'
-,p_last_upd_yyyymmddhh24miss=>'20211001085231'
+,p_last_upd_yyyymmddhh24miss=>'20211001090104'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(26262013095397178)
@@ -36,6 +74,25 @@ wwv_flow_api.create_page_plug(
 ,p_attribute_02=>'HTML'
 ,p_attribute_03=>'Y'
 );
-wwv_flow_api.component_end;
+wwv_flow_api.create_page_plug(
+ p_id=>wwv_flow_api.id(26300783091446901)
+,p_plug_name=>'Title 1'
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_plug_template=>wwv_flow_api.id(26161693516397044)
+,p_plug_display_sequence=>10
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_display_point=>'BODY'
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+,p_attribute_01=>'N'
+,p_attribute_02=>'HTML'
+);
 end;
 /
+prompt --application/end_environment
+begin
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
